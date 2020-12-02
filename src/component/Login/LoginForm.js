32d0e3f465dad703ce81af69704a7f8254418f80
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-12-02 14:24:41 -0300

    useForm 808 Fazendo um hook externo

--- src/component/Login/LoginForm.js
+++ src/component/Login/LoginForm.js
@@ -1,38 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useForm from '../../Hooks/useForm'
 import Button from '../forms/Button'
 import Input from '../forms/Input'
 
 function LoginForm() {
-    const [username, setUsername] = React.useState("")
-    const[password, setPassword]= React.useState('')
 
+   const username = useForm();
+   const password = useForm();
+   
     function handleSubmit(event){
         event.preventDefault()
-        fetch('https://dogsapi.origamid.dev/jason/jwt-auth/v1/token',{
-            method:'POST',
-            headers:{
-                'Content-Type':'Aplication/json',
-            },
-            body: JSON.stringify({username,password})
-        })
-        .then((response)=> {
-            console.log(response)
-            return response.json()
-        })
-        .then(json => {
-            console.logO(json)
-        })
+
+        if(username.validate && password.validate){
+            fetch('https://dogsapi.origamid.dev/jason/jwt-auth/v1/token',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'Aplication/json',
+                },
+                body: JSON.stringify({username,password})
+            })
+            .then((response)=> {
+                console.log(response)
+                return response.json()
+            })
+            .then(json => {
+                console.logO(json)
+            })
+        }
     }
+
   
 
 
     return (
-        <section>
+        <section className="container">
             <h1>Login</h1>
             <form action="" onSubmit={handleSubmit}>
-                <Input type="text" name="username" label="Name"/>
-                <Input type="password" name="password" label="Senha"/>
+                <Input type="text" name="username" label="Name" {...username}/>
+                <Input type="password" name="password" label="Senha" {...password}/>
                 <Button children="Entrar"/>
             </form>
             <Link to="/login/criar">Cadastro</Link>
--- src/component/forms/Input.js
+++ src/component/forms/Input.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function input({label,type,name}) {
+function input({label, type, name, value, onChange, error, onBlur}) {
     return (
         <div>
             <label htmlFor={name}>{label}</label>
-            <input name={name} id={name}type={type}/>
-            <p className="erro">Error</p>
+            <input name={name} id={name}type={type} onChange={onChange} value={value} onBlur={onBlur}/>
+    { error && <p className="erro">{error}</p>}
         </div>
     )
 }
